2f5b01f21dc94b559ae0d33b2c8dacd7f3a91693
palukuruchandu <chandu1010chandu@gmail.com>
2022-03-09 13:33:07 +0530

    ok

--- product.js
+++ product.js
@@ -1,15 +1,16 @@
 var brand = "APPLE";
 var name1 = "APPLE IPHONE SE 2020";
 var price = 39999;
- var rating = 4.9;
- var discount = true;
- var stock;
- console.log(type of brand)
- console.log(type of name1)
- console.log(type of price)
- console.log(type of rating)
- console.log(type of discount)
- console.log( type of stock)
+var rating = 4.9;
+var discount = true;
+var stock;
+
+console.log(typeof brand)
+console.log(typeof name1)
+console.log(typeof price)
+console.log(typeof rating)
+console.log(typeof discount)
+console.log(typeof stock)
 
 
 
